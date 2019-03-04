df641da1fce485168ffc5d71360fdcbbd9be3c2e
ingabire <ingabire@sylvie.gmail.com>
2019-03-04 13:51:53 +0200

    my app have the blog posts on the site

--- models.py
+++ models.py
@@ -1,112 +1,112 @@
-from . import db
-from werkzeug.security import generate_password_hash,check_password_hash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from . import login_manager
-from datetime import datetime
+# from . import db
+# from werkzeug.security import generate_password_hash,check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import UserMixin
+# from . import login_manager
+# from datetime import datetime
 
-class User(UserMixin,db.Model):
-    __tablename__='users'
+# class User(UserMixin,db.Model):
+#     __tablename__='users'
 
-    id = db.Column(db.Integer,primary_key=True)
-    username= db.Column(db.String(255),index = True)
-    email = db.Column(db.String(255),unique = True , index=True)
-    bio= db.Column(db.String(255))
-    profile_pic_path = db.Column(db.String(255))
-    pass_secure=db.Column(db.String(255))
-    pitches= db.relationship('Pitch',backref='user', lazy='dynamic')
-    comments = db.relationship('Comment',backref='user' ,lazy='dynamic')
+#     id = db.Column(db.Integer,primary_key=True)
+#     username= db.Column(db.String(255),index = True)
+#     email = db.Column(db.String(255),unique = True , index=True)
+#     bio= db.Column(db.String(255))
+#     profile_pic_path = db.Column(db.String(255))
+#     pass_secure=db.Column(db.String(255))
+#     pitches= db.relationship('Pitch',backref='user', lazy='dynamic')
+#     comments = db.relationship('Comment',backref='user' ,lazy='dynamic')
 
-    @property
-    def password(self):
-        raise AttributeError('You cannot read the password attribute')
+#     @property
+#     def password(self):
+#         raise AttributeError('You cannot read the password attribute')
 
-    @password.setter
-    def password(self, password):
-        self.pass_secure = generate_password_hash(password)
-
-
-    def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
-
-    def __repr__(self):
-        return f'User {self.username}'
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+#     @password.setter
+#     def password(self, password):
+#         self.pass_secure = generate_password_hash(password)
 
 
-class quote(db.Model):
-   __tablename__ = 'quotes'
+#     def verify_password(self,password):
+#         return check_password_hash(self.pass_secure,password)
 
-   id = db.Column(db.Integer,primary_key = True)
-   user_id= db.Column(db.Integer,db.ForeignKey('users.id'))
-   content = db.Column(db.String(255))
-   comments = db.relationship('Comment',backref='pitch' ,lazy='dynamic')
-   category = db.Column(db.String(255))
-#    upvotes = db.Column(db.Integer)
-#    downvotes = db.Column(db.Integer)
+#     def __repr__(self):
+#         return f'User {self.username}'
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 
-   def save_quote(self):
-        db.session.add(self)
-        db.session.commit()
+# class quote(db.Model):
+#    __tablename__ = 'quotes'
 
-   @classmethod
-   def clear_quote(cls):
-        quote.all_quotes.clear()
+#    id = db.Column(db.Integer,primary_key = True)
+#    user_id= db.Column(db.Integer,db.ForeignKey('users.id'))
+#    content = db.Column(db.String(255))
+#    comments = db.relationship('Comment',backref='pitch' ,lazy='dynamic')
+#    category = db.Column(db.String(255))
+# #    upvotes = db.Column(db.Integer)
+# #    downvotes = db.Column(db.Integer)
 
-   @classmethod
-   def get_quote(cls,id):
-        quote = quote.query.filter_by(id=id).all()
-        return quote
-    
-   @classmethod
-   def get_quotes(cls):
-       quotes = quote.query.filter_by().all()
-       return quotes
+
+#    def save_quote(self):
+#         db.session.add(self)
+#         db.session.commit()
 
 #    @classmethod
-#    def upvotess(cls,id):
-#        pitch=Pitch.query.filter_by(id=id).first()
-#        pitch.upvotes=0
-#        upvotes=pitch.upvotes+1
-#        return upvotes
+#    def clear_quote(cls):
+#         quote.all_quotes.clear()
 
 #    @classmethod
-#    def downvotess(cls,id):
-#        pitch=Pitch.query.filter_by(id=id).first()
-#        pitch.downvotes=0
-#        downvotes=pitch.downvotes-1
-#        return downvotes
-
-
-
-
-class Comment(db.Model):
-    __tablename__= 'comments'
+#    def get_quote(cls,id):
+#         quote = quote.query.filter_by(id=id).all()
+#         return quote
     
-    id= db.Column(db.Integer,primary_key= True)
-    content = db.Column(db.String(255))
-    user_id= db.Column(db.Integer,db.ForeignKey('users.id'))
-    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+#    @classmethod
+#    def get_quotes(cls):
+#        quotes = quote.query.filter_by().all()
+#        return quotes
+
+# #    @classmethod
+# #    def upvotess(cls,id):
+# #        pitch=Pitch.query.filter_by(id=id).first()
+# #        pitch.upvotes=0
+# #        upvotes=pitch.upvotes+1
+# #        return upvotes
+
+# #    @classmethod
+# #    def downvotess(cls,id):
+# #        pitch=Pitch.query.filter_by(id=id).first()
+# #        pitch.downvotes=0
+# #        downvotes=pitch.downvotes-1
+# #        return downvotes
 
 
-    def save_comment(self):
-        db.session.add(self)
-        db.session.commit()
 
-    @classmethod
-    def clear_comments(cls):
-        Comment.all_comments.clear()
 
-    @classmethod
-    def get_comments(cls,id):
-        comments = Comment.query.filter_by(pitch_id=id).all()
-        return comments
+# class Comment(db.Model):
+#     __tablename__= 'comments'
+    
+#     id= db.Column(db.Integer,primary_key= True)
+#     content = db.Column(db.String(255))
+#     user_id= db.Column(db.Integer,db.ForeignKey('users.id'))
+#     pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
 
-    @classmethod
-    def get_commentss(cls,id):
-        comments = Comment.query.filter_by(user_id=id).all()
-        return comments
+
+#     def save_comment(self):
+#         db.session.add(self)
+#         db.session.commit()
+
+#     @classmethod
+#     def clear_comments(cls):
+#         Comment.all_comments.clear()
+
+#     @classmethod
+#     def get_comments(cls,id):
+#         comments = Comment.query.filter_by(pitch_id=id).all()
+#         return comments
+
+#     @classmethod
+#     def get_commentss(cls,id):
+#         comments = Comment.query.filter_by(user_id=id).all()
+#         return comments
